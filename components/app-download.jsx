3b53204bb4aa20f038bbd12d9b6f2fbe668de934
Faisal baig <fasalbaig255@gmail.com>
2025-01-30 17:14:48 +0530

    app-download

--- components/app-download.jsx
+++ components/app-download.jsx
@@ -70,7 +70,7 @@ export function AppDownloadComponent() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.5 }}
-              className="flex justify-center gap-8"
+              className="flex space-x-14"
             >
               <Button 
                 className="bg-black hover:bg-gray-800 text-white px-3 py-6 rounded-md"
